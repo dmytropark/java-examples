8de46faf15ae74c2b5d162cc583adeed24ab3185
Dmytro <dimagit24@gmail.com>
2020-03-05 00:56:48 +0200

    nio test for 'listFiles' method

--- Java-core/src/test/java/com/parkhomenko/nio/WorkWithNIO.java
+++ Java-core/src/test/java/com/parkhomenko/nio/WorkWithNIO.java
@@ -2,13 +2,21 @@ package com.parkhomenko.nio;
 
 import org.junit.Test;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.nio.file.*;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.UUID;
+import java.util.stream.Stream;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
+/**
+ * https://www.baeldung.com/java-nio-2-file-api
+ */
 public class WorkWithNIO {
 
     private static String HOME = System.getProperty("user.home");
@@ -160,8 +168,7 @@ public class WorkWithNIO {
 
     @Test(expected = DirectoryNotEmptyException.class)
     public void givenPath_whenFailsToDeleteNonEmptyDir_thenCorrect() throws IOException {
-        Path dir = Paths.get(
-                HOME + "/emptyDir" + UUID.randomUUID().toString());
+        Path dir = Paths.get(HOME + "/emptyDir" + UUID.randomUUID().toString());
         Files.createDirectory(dir);
         assertTrue(Files.exists(dir));
 
@@ -172,4 +179,67 @@ public class WorkWithNIO {
 
         assertTrue(Files.exists(dir));
     }
+
+    @Test
+    public void givenFilePath_whenCopiesToNewLocation_thenCorrect() throws IOException {
+        Path dir1 = Paths.get(HOME + "/firstdir_" + UUID.randomUUID().toString());
+        Path dir2 = Paths.get(HOME + "/otherdir_" + UUID.randomUUID().toString());
+
+        Files.createDirectory(dir1);
+        Files.createDirectory(dir2);
+
+        Path file1 = dir1.resolve("filetocopy.txt");
+        Path file2 = dir2.resolve("filetocopy.txt");
+
+        Files.createFile(file1);
+
+        assertTrue(Files.exists(file1));
+        assertFalse(Files.exists(file2));
+
+        Files.copy(file1, file2);
+
+        assertTrue(Files.exists(file2));
+    }
+
+    @Test(expected = FileAlreadyExistsException.class)
+    public void givenPath_whenCopyFailsDueToExistingFile_thenCorrect() throws IOException {
+        Path dir1 = Paths.get(HOME + "/firstdir_" + UUID.randomUUID().toString());
+        Path dir2 = Paths.get(HOME + "/otherdir_" + UUID.randomUUID().toString());
+
+        Files.createDirectory(dir1);
+        Files.createDirectory(dir2);
+
+        Path file1 = dir1.resolve("filetocopy.txt");
+        Path file2 = dir2.resolve("filetocopy.txt");
+
+        Files.createFile(file1);
+        Files.createFile(file2);
+
+        assertTrue(Files.exists(file1));
+        assertTrue(Files.exists(file2));
+
+        Files.copy(file1, file2);
+
+        Files.copy(file1, file2, StandardCopyOption.REPLACE_EXISTING);
+    }
+
+    @Test
+    public void givenFolder_whenGetListOfFilesInFolder_thenCorrect() throws URISyntaxException {
+        URL resource = this.getClass().getClassLoader().getResource("test-files");
+
+        Path path = Paths.get(resource.toURI());
+
+        assertTrue(Files.exists(path));
+
+        File[] files = path.toFile().listFiles();
+
+        HashSet set = new HashSet<>(Arrays.asList("1.txt", "2.txt"));
+
+        boolean res = Stream.of(files).allMatch(file -> set.contains(file.getName()));
+
+        assertEquals(2,files.length);
+        assertTrue(res);
+
+    }
+
 }
